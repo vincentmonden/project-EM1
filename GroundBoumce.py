7283d2e084a2c25784a3ec521ff8411fe25c0e71
pmoiy <marco.vanderpoorten@gmail.com>
2024-12-09 10:08:14 +0100

    werkende simulatie!!

--- GroundBoumce.py
+++ GroundBoumce.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as pl
 import json
+from scipy.spatial import distance
 
 #####################################################################################################
 #----------------------------------------------------------------------------------------------------
@@ -13,21 +14,27 @@ with open("init.json") as f:
 
 J0 = variables["J0"]/1000000 #/1000000 => change input to correct units
 jsigma = variables["jsigma"]
-tc = 5*jsigma               #units: coulomb
+tc = 8*jsigma               #units: coulomb
 epsilon = 8.854*variables["epsr"]            #megaAmpere
 mu = 1.2566/1000000 * variables["mur"]        #teravolt, microsecond
 
+d = variables["dist"]
 lambdamin = 2 * np.pi * jsigma / 3 / np.sqrt(mu * epsilon) 
 deltax = lambdamin * variables["deltax"] #in meter?
 deltay = lambdamin * variables["deltay"]
 deltat = np.sqrt(epsilon*mu)/np.sqrt(1/deltax**2 + 1/deltay**2)*variables["deltat"]#Units: second?
 
+voltpos = (int(np.floor(variables["voltpos"][0]/deltax)), int(np.floor(variables["voltpos"][1]/deltay)))
+Jpos = (int(np.floor(variables["jpos"][0]/deltax)), int(np.floor(variables["jpos"][1]/deltay)))
+
 lenx = variables["xlen"] - variables["xlen"] % deltax #Grootte heel het spel[m]
 leny = variables["ylen"] - variables["ylen"] % deltay
-lent = min([lenx,leny])*np.sqrt(epsilon*mu)- min([lenx,leny])*np.sqrt(epsilon*mu) % deltat#[microsec]
+lengtht = np.min([distance.euclidean(variables["jpos"], [2*lenx - variables["voltpos"][0], variables["voltpos"][1]]), distance.euclidean(variables["jpos"], [variables["voltpos"][0], 2*leny - variables["voltpos"][1]]), distance.euclidean(variables["jpos"], [variables["voltpos"][0], - variables["voltpos"][1]]), distance.euclidean(variables["jpos"], [- variables["voltpos"][0], variables["voltpos"][1]])])
+print(lengtht)
+lent = lengtht*np.sqrt(epsilon*mu)- lengtht*np.sqrt(epsilon*mu) % deltat#[microsec]
 #verbeter voor dichtste pad met bounce
 
-Jpos = (int(np.floor(variables["jpos"][0]/deltax)), int(np.floor(variables["jpos"][1]/deltay)))
+
 caplist = variables["capa"]#[picofarad]
 for cap in caplist:
     cap[0] = int(np.floor(cap[0]/deltax)) #change inputto correct units
@@ -35,8 +42,7 @@ for cap in caplist:
 
 amxpoints = int(lenx/deltax) +1 #aantal xpunten
 amypoints = int(leny/deltay) +1 #+1 om beide edges mee te rekenen
-amtpoints = int(lent/deltat) +1
-#amtpoints = min([amxpoints, amypoints]) #Simulatie stopt op tijd: niet juist, gebruik phase velocity
+amtpoints = int(lent/deltat) -30
 print(f"The simulation has {amtpoints} timesteps")
 
 assert amxpoints*amypoints*amtpoints < 8000000000, f'These time- and space steps take at least more than 6GB of RAM, so watch out!! Only change this assert when confident in computing power!!'
@@ -47,8 +53,6 @@ magny = np.zeros((amtpoints, amxpoints - 1, amypoints)) #magnetisch veld -1 omda
 
 Jsource = J0*np.exp(-1*(np.linspace(0, lent, amtpoints) - tc)**2/(2*jsigma**2)) #Alle berekeningen van de source vectorised voor de loop
 
-d = variables["dist"]
-
 #############################################################################################################################################################
 #------------------------------------------------------------------------------------------------------------------------------------------------------------
 #SIMULATION
@@ -56,6 +60,7 @@ d = variables["dist"]
 #############################################################################################################################################################
 
 #Deze waardes veranderen niet doorheen de simulatie -> Maar 1 keer berekenen
+
 coef1 = (deltat/(deltax * mu))
 coef2 = (deltat/(deltay * mu))
 coef3 = np.full((amxpoints-2,amypoints-2),(deltat/(deltax * epsilon))) #niet ideaal om een array te maken voor alle coefficienten, maar ik zie niet drect een manier om dit te vereenvoudigen zonder for-loops te itroduceren
@@ -63,8 +68,9 @@ coef4 = np.full((amxpoints-2,amypoints-2),(deltat/(deltay * epsilon)))
 coef5 = deltat / epsilon / deltax / deltay
 
 for cap in caplist:
-    coef3[cap[0]][cap[1]] = 1/(1/coef3[cap[0]][cap[1]] - deltax*cap[2]*d/deltat)
-    coef4[cap[0]][cap[1]] = 1/(1/coef4[cap[0]][cap[1]] - deltax*cap[2]*d/deltat)
+    coefhulp = epsilon - (cap[2]*d / deltax/deltay)
+    coef3[cap[0]][cap[1]] = deltat/(deltax * coefhulp)
+    coef4[cap[0]][cap[1]] = deltat/(deltay * coefhulp)
     print(deltax/deltat)
     print(coef4[cap[0]][cap[1]])
 
@@ -84,12 +90,15 @@ for tijdstip in range(1, amtpoints):
 #------------------------------------------------------------------------------------------------------------------------------------------------------------
 #############################################################################################################################################################
 
-
+pl.rcParams['text.usetex'] = True
 #print(magny[:,57,57][2])
 #print(magny[:,57,56][2])
 #pl.plot(magny[:,int(np.floor(amxpoints/4)),int(np.floor(amxpoints/4))])
 #pl.plot(magny[:,int(np.floor(amxpoints/4)) - 1,int(np.floor(amxpoints/4))], linestyle='dotted')
-pl.plot(elecz[:,127,127])
+timelist = deltat*np.linspace(0, amtpoints, amtpoints)
+pl.plot(timelist,d*elecz[:,voltpos[0],voltpos[1]]*1000000000000, color='navy')
+pl.ylabel(r"$\hat{v}[V]$")
+pl.xlabel(r"$t[\mu s]$")
 #pl.plot(magny[:,57,57])
 #pl.xlim([0,30])
 pl.show()
@@ -103,5 +112,6 @@ pl.show()
 #
 # TO DO:
 # -> source terms + check leapfrog uit verandering va stapgrootte)
-# -> calculate voltages
+# -> max t verbeteren (geen bouncy)
 # -> surface transfer impedance
+# -> 
--- README.txt
+++ README.txt
@@ -17,3 +17,20 @@ capa: input for capacitors, add lists inside this list to include more capacitor
     The form: [xposition(meter), yposition(meter), capacitance(pico farad)]
 
 dist: distance between the plates (in meter)
+voltpos: point where the voltage is wanted to be measured. (in meter)
+
+{
+    "epsr": 1, 
+    "mur": 1,
+    "xlen": 0.1,
+    "ylen": 0.2,
+    "J0": 1,
+    "jsigma": 0.00000000001,
+    "jpos": [0.05, 0.05],
+    "deltax": 0.05,
+    "deltay": 0.05,
+    "deltat": 0.5,
+    "capa": [[0.07, 0.07, 0.00000000001],[0.04,0.04,0.000001]],
+    "dist": 0.001
+}
+,[0.04,0.04,1000000000]
